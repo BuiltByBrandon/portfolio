2ee5b01ad0c9b5b4a256dbfc4ce8097a020a41bd
BuiltByBrandon <guzman.brandon12@Live.com>
2022-06-08 20:30:56 +0000

    fix: changed page metadata to relevant data

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,10 +5,9 @@ import { openGraph } from '@/lib/helper';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
-  description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+  title: 'Brandon Guzman-Morales Portfolio',
+  siteName: 'Brandon Guzman-Morales Portfolio',
+  description: 'A website for Brandon Guzman-Morales. A portfolio of his work.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://tsnext-tw.thcl.dev',
   type: 'website',
